0ebd157b9bc3dbcff86e5cb77baa8c4dd2b7fb09
HajarZiya <116388510+hajarziya@users.noreply.github.com>
2023-12-01 13:17:13 +0300

    feat: edit category

--- src/templates/admin/categories/CategoriesPage.tsx
+++ src/templates/admin/categories/CategoriesPage.tsx
@@ -1,10 +1,36 @@
 import { AdminLayout } from '@src/components/admin'
 import useStyles from './CategoriesPage.styles'
-import { Box, Pagination, Stack, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material'
+import {
+  Box,
+  Button,
+  Pagination,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+  Popover
+} from '@mui/material'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
+import React from 'react'
+import EditNoteIcon from '@mui/icons-material/EditNote'
 
 export function CategoriesPage () {
   const { classes } = useStyles()
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  const open = Boolean(anchorEl)
+  const id = <MoreVertIcon/> ? 'Edit' : undefined
+
   return (
 		<>
 			<AdminLayout>
@@ -16,7 +42,22 @@ export function CategoriesPage () {
 							<TableRow>
 								<TableCell>Title</TableCell>
 								<TableCell>Active</TableCell>
-								<TableCell>Actions</TableCell>
+								<TableCell>
+									<Button aria-describedby={id} variant="contained" onClick={handleClick}><MoreVertIcon/></Button>
+									<Popover
+										className={classes.popver}
+										id={id}
+										open={open}
+										anchorEl={anchorEl}
+										onClose={handleClose}
+										anchorOrigin={{
+										  vertical: 'bottom',
+										  horizontal: 'left'
+										}}
+									>
+										<Typography sx={{ padding: '1px 10px' }}><EditNoteIcon/> Edit</Typography>
+									</Popover>
+								</TableCell>
 							</TableRow>
 						</TableHead>
 						<TableBody>
@@ -24,63 +65,180 @@ export function CategoriesPage () {
 								<TableCell>Fantastic</TableCell>
 								<TableCell>Yes</TableCell>
 								<TableCell>
-									<MoreVertIcon/>
+									<Button aria-describedby={id} variant="contained" onClick={handleClick}><MoreVertIcon/></Button>
+									<Popover
+										className={classes.popver}
+										id={id}
+										open={open}
+										anchorEl={anchorEl}
+										onClose={handleClose}
+										anchorOrigin={{
+										  vertical: 'bottom',
+										  horizontal: 'left'
+										}}
+									>
+										<Typography sx={{ padding: '1px 10px' }}><EditNoteIcon/> Edit</Typography>
+									</Popover>
 								</TableCell>
 							</TableRow>
 							<TableRow>
 								<TableCell>Fantastic</TableCell>
 								<TableCell>Yes</TableCell>
 								<TableCell>
-									<MoreVertIcon/>
+									<Button aria-describedby={id} variant="contained" onClick={handleClick}><MoreVertIcon/></Button>
+									<Popover
+										className={classes.popver}
+										id={id}
+										open={open}
+										anchorEl={anchorEl}
+										onClose={handleClose}
+										anchorOrigin={{
+										  vertical: 'bottom',
+										  horizontal: 'left'
+										}}
+									>
+										<Typography sx={{ padding: '1px 10px' }}><EditNoteIcon/> Edit</Typography>
+									</Popover>
 								</TableCell>
 							</TableRow>
 							<TableRow>
 								<TableCell>Fantastic</TableCell>
 								<TableCell>Yes</TableCell>
 								<TableCell>
-									<MoreVertIcon/>
+									<Button aria-describedby={id} variant="contained" onClick={handleClick}><MoreVertIcon/></Button>
+									<Popover
+										className={classes.popver}
+										id={id}
+										open={open}
+										anchorEl={anchorEl}
+										onClose={handleClose}
+										anchorOrigin={{
+										  vertical: 'bottom',
+										  horizontal: 'left'
+										}}
+									>
+										<Typography sx={{ padding: '1px 10px' }}><EditNoteIcon/> Edit</Typography>
+									</Popover>
 								</TableCell>
 							</TableRow>
 							<TableRow>
 								<TableCell>Fantastic</TableCell>
 								<TableCell>Yes</TableCell>
 								<TableCell>
-									<MoreVertIcon/>
+									<Button aria-describedby={id} variant="contained" onClick={handleClick}><MoreVertIcon/></Button>
+									<Popover
+										className={classes.popver}
+										id={id}
+										open={open}
+										anchorEl={anchorEl}
+										onClose={handleClose}
+										anchorOrigin={{
+										  vertical: 'bottom',
+										  horizontal: 'left'
+										}}
+									>
+										<Typography sx={{ padding: '1px 10px' }}><EditNoteIcon/> Edit</Typography>
+									</Popover>
 								</TableCell>
 							</TableRow>
 							<TableRow>
 								<TableCell>Fantastic</TableCell>
 								<TableCell>Yes</TableCell>
 								<TableCell>
-									<MoreVertIcon/>
+									<Button aria-describedby={id} variant="contained" onClick={handleClick}><MoreVertIcon/></Button>
+									<Popover
+										className={classes.popver}
+										id={id}
+										open={open}
+										anchorEl={anchorEl}
+										onClose={handleClose}
+										anchorOrigin={{
+										  vertical: 'bottom',
+										  horizontal: 'left'
+										}}
+									>
+										<Typography sx={{ padding: '1px 10px' }}><EditNoteIcon/> Edit</Typography>
+									</Popover>
 								</TableCell>
 							</TableRow>
 							<TableRow>
 								<TableCell>Fantastic</TableCell>
 								<TableCell>Yes</TableCell>
 								<TableCell>
-									<MoreVertIcon/>
+									<Button aria-describedby={id} variant="contained" onClick={handleClick}><MoreVertIcon/></Button>
+									<Popover
+										className={classes.popver}
+										id={id}
+										open={open}
+										anchorEl={anchorEl}
+										onClose={handleClose}
+										anchorOrigin={{
+										  vertical: 'bottom',
+										  horizontal: 'left'
+										}}
+									>
+										<Typography sx={{ padding: '1px 10px' }}><EditNoteIcon/> Edit</Typography>
+									</Popover>
 								</TableCell>
 							</TableRow>
 							<TableRow>
 								<TableCell>Fantastic</TableCell>
 								<TableCell>Yes</TableCell>
 								<TableCell>
-									<MoreVertIcon/>
+									<Button aria-describedby={id} variant="contained" onClick={handleClick}><MoreVertIcon/></Button>
+									<Popover
+										className={classes.popver}
+										id={id}
+										open={open}
+										anchorEl={anchorEl}
+										onClose={handleClose}
+										anchorOrigin={{
+										  vertical: 'bottom',
+										  horizontal: 'left'
+										}}
+									>
+										<Typography sx={{ padding: '1px 10px' }}><EditNoteIcon/> Edit</Typography>
+									</Popover>
 								</TableCell>
 							</TableRow>
 							<TableRow>
 								<TableCell>Fantastic</TableCell>
 								<TableCell>Yes</TableCell>
 								<TableCell>
-									<MoreVertIcon/>
+									<Button aria-describedby={id} variant="contained" onClick={handleClick}><MoreVertIcon/></Button>
+									<Popover
+										className={classes.popver}
+										id={id}
+										open={open}
+										anchorEl={anchorEl}
+										onClose={handleClose}
+										anchorOrigin={{
+										  vertical: 'bottom',
+										  horizontal: 'left'
+										}}
+									>
+										<Typography sx={{ padding: '1px 10px' }}><EditNoteIcon/> Edit</Typography>
+									</Popover>
 								</TableCell>
 							</TableRow>
 							<TableRow>
 								<TableCell>Fantastic</TableCell>
 								<TableCell>Yes</TableCell>
 								<TableCell>
-									<MoreVertIcon/>
+									<Button aria-describedby={id} variant="contained" onClick={handleClick}><MoreVertIcon/></Button>
+									<Popover
+										className={classes.popver}
+										id={id}
+										open={open}
+										anchorEl={anchorEl}
+										onClose={handleClose}
+										anchorOrigin={{
+										  vertical: 'bottom',
+										  horizontal: 'left'
+										}}
+									>
+										<Typography sx={{ padding: '1px 10px' }}><EditNoteIcon/> Edit</Typography>
+									</Popover>
 								</TableCell>
 							</TableRow>
 						</TableBody>
