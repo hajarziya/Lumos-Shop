024c6d8eb050b93bab66c7ecc471c3bb4b7c1cac
HajarZiya <116388510+hajarziya@users.noreply.github.com>
2024-01-03 20:09:43 +0300

    fix:style and validation login page

--- src/templates/admin/login/validation.ts
+++ src/templates/admin/login/validation.ts
@@ -1,15 +1,9 @@
 import { object, string } from 'yup'
 
 export const LoginSchema = object({
-  username: string().required('Username is required')
-    .min(4, 'Password must be at least 4 characters')
-    .max(12, 'Password must not exceed 12 characters'),
+  username: string().required('Username is required'),
+
   password: string()
     .required('Password is required')
-    .min(4, 'Password must be at least 4 characters')
-    .max(12, 'Password must not exceed 12 characters')
-    .matches(
-      /^(?=.*[a-z])(?=.*\d)/,
-      'Password must contain at least one letter and one number'
-    )
+
 })
